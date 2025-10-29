24f3b37551f1f4b7ea777641e5356753e2a88ed6
secile <secile.develop@hotmail.com>
2025-10-29 11:13:42 +0900

    add UsbCameraByteArray sample project that shows how to use Y16.

---
+++ SampleProject/UsbCameraByteArray/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿
+namespace UsbCameraByteArray
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(291, 136);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 29);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Still Image";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(291, 100);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 29);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "GetBitmap";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(399, 15);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(266, 187);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(16, 15);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(266, 187);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(685, 326);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ SampleProject/UsbCameraByteArray/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GitHub.secile.Video;
+
+// This is a sample of using gray scale camera that supports Y8, Y800, and Y16 video format.
+// This program is tested on the product of TheImagingSource USB camera.
+// 
+// Y800(and Y8) is an 8-bit, Y16 is a 16-bit grayscale format.
+// To use these video format, please follow the steps below.
+//
+// (1) define 'USBCAMERA_BYTEARRAY' symbol, that makes GetBitmap() returns image data as byte array.
+//     in case on WPF, define 'USBCAMERA_WPF' symbol at a same time.
+//
+// (2) to preview the image, do not use SetPreviewControl on WinForms.
+//     use Timer and GetBitmap() on WinForms, or use PreviewCaptured on WPF.
+//
+// (3) to show the image, you have to convert byte array to Bitmap(on WinForms) or BitmapSource(on WPF),
+//     use UsbCamera.ByteArrayUtility.Y800.CreateBitmap or UsbCamera.ByteArrayUtility.Y16.CreateBitmap function.
+//
+// note on using Y16 video format.
+//
+// (4) Y16 is a 16-bit grayscale format, but in general, C# Bitmap class is not able to handle 16-bit grayscale image properly.
+//     UsbCamera.ByteArrayUtility.Y16.CreateBitmap returns 8-bit grayscale image, which causes the lost of pixel bit depth.
+//     do not use the Bitmap to inspect the luminance value of image, use this only for preview.
+//
+// (5) to inspect/manipulate the luminance value of specific coordinates from byte array,
+//     use UsbCamera.ByteArrayUtility.Y800.GetValue/SetValue or UsbCamera.ByteArrayUtility.Y16.GetValue/SetValue function.
+//
+// (6) Y16 video format has it's own bit depth(8, 10 or 12) and it's depend of your cameras spec.
+//     you must specify the bit depth as an argument when you call the UsbCamera.ByteArrayUtility.Y16.CreateBitmap, GetValue, SetValue function.
+
+namespace UsbCameraByteArray
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // find device.
+            var devices = UsbCamera.FindDevices();
+            if (devices.Length == 0) return; // no device.
+
+            // get video format.
+            var cameraIndex = 0;
+            var formats = UsbCamera.GetVideoFormat(cameraIndex);
+
+            // select the format you want.
+            for (int i = 0; i < formats.Length; i++) Console.WriteLine("{0}:{1}", i, formats[i]);
+
+            // *** select Y16 video format here. ***
+            // if you use Y800, select Y800, and use UsbCamera.ByteArrayUtility.Y800 functions.
+            var format = formats[0];
+
+            // create instance.
+            var camera = new UsbCamera(cameraIndex, format);
+            // this closing event handler make sure that the instance is not subject to garbage collection.
+            this.FormClosing += (s, ev) => camera.Release(); // release when close.
+
+            // show preview
+            var timer = new System.Timers.Timer(1000 / 30) { SynchronizingObject = this };
+            timer.Elapsed += (s, ev) =>
+            {
+                var buffer = (byte[])camera.GetBitmap();
+                var bmp = UsbCamera.ByteArrayUtility.Y16.CreateBitmap(buffer, 12, camera.Size.Width, camera.Size.Height);
+                pictureBox1.Image = bmp;
+            };
+
+            timer.Start();
+            this.FormClosing += (s, ev) => timer.Stop();
+
+            // start.
+            camera.Start();
+
+            // get bitmap.
+            button1.Click += (s, ev) =>
+            {
+                var buffer = (byte[])camera.GetBitmap();
+
+                // get/set luminance value sample.
+                // execute binarization.
+                for (int y = 0; y < camera.Size.Height; y++)
+                {
+                    for (int x = 0; x < camera.Size.Width; x++)
+                    {
+                        var value = UsbCamera.ByteArrayUtility.Y16.GetValue(buffer, 12, camera.Size.Width, camera.Size.Height, x, y);
+                        value = (ushort)(value > 0x0FFF / 2 ? 0x0FFF : 0);
+                        UsbCamera.ByteArrayUtility.Y16.SetValue(buffer, 12, camera.Size.Width, camera.Size.Height, x, y, value);
+                    }
+                }
+
+                pictureBox2.Image = UsbCamera.ByteArrayUtility.Y16.CreateBitmap(buffer, 12, camera.Size.Width, camera.Size.Height);
+            };
+        }
+    }
+}
